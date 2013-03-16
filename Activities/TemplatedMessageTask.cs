07066c53b3a50be42fa9dc556a2599f0c09f197f
GQAdonis <gqadonis@me.com>
2013-03-16 07:00:37 -0500

    messaging

---
+++ Activities/TemplatedMessageTask.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.Localization;
+using Orchard.Messaging.Services;
+using Orchard.Workflows.Services;
+
+namespace DarkSky.Messaging.Activities
+{
+    public class TemplatedMessageTask : Task
+    {
+        private readonly IMessageManager _messageManager;
+        public TemplatedMessageTask(IMessageManager messageManager)
+        {
+            _messageManager = messageManager;
+            T = NullLocalizer.Instance;
+        }
+
+        public Localizer T { get; set; }
+
+        public override string Name
+        {
+            get { return "TemplatedMessageTask"; }
+        }
+
+        public override Orchard.Localization.LocalizedString Category
+        {
+            get { return T("Messaging"); }
+        }
+
+        public override Orchard.Localization.LocalizedString Description
+        {
+            get { return T("Send Templated Messages"); }
+        }
+
+        public override IEnumerable<Orchard.Localization.LocalizedString> GetPossibleOutcomes(Orchard.Workflows.Models.WorkflowContext workflowContext, Orchard.Workflows.Models.ActivityContext activityContext)
+        {
+            yield return T("Sent");
+        }
+
+        public override IEnumerable<Orchard.Localization.LocalizedString> Execute(Orchard.Workflows.Models.WorkflowContext workflowContext, Orchard.Workflows.Models.ActivityContext activityContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
